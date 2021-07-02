23a3c3474a328d20c395822ebaafdd36c3f0869a
github username <67525690+kirstybrews@users.noreply.github.com>
2021-07-02 12:58:24 -0600

    wrote first test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,9 +3,10 @@ require_relative "../lib/person"
 describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
+      person = Person.new(first_name: 'Kirsty', last_name: 'Brewster')
+      expect(person.full_name).to eq('Kirsty Brewster')
       # Write your spec here.
       # You can delete the pending line.
-      pending "Not written yet"
     end
   end
 end
